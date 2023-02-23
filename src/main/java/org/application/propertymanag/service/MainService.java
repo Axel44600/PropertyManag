564aa5e1d1529684cdfd2007277ea6caeb1bc04f
= <=>
2023-02-23 14:28:34 +0100

    Added MainService and MainServiceImpl

---
+++ src/main/java/org/application/propertymanag/service/MainService.java
@@ -0,0 +1,16 @@
+package org.application.propertymanag.service;
+
+public interface MainService {
+    public boolean verifDigit(String var);
+
+    public boolean verifSpecialChar(String var);
+
+    public String maj(String var);
+
+    public boolean verifMaj(String var);
+
+    public boolean verifSize(String var, Integer minSize);
+
+    public boolean verifFormatTel(String tel);
+
+}
---
+++ src/main/java/org/application/propertymanag/service/impl/MainServiceImpl.java
@@ -0,0 +1,69 @@
+package org.application.propertymanag.service.impl;
+
+import org.application.propertymanag.service.MainService;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Character.isDigit;
+
+@Service
+public class MainServiceImpl implements MainService {
+
+    public boolean verifDigit(String var) {
+        boolean number = false;
+        for(int a = 0; a < var.length(); a++) {
+            char l = var.charAt(a);
+            if(isDigit(l)) {
+                number = true;
+            }
+        }
+        if(number) { return true; } else { return false; }
+    }
+
+    public boolean verifSpecialChar(String var) {
+        Pattern pattern = Pattern.compile("\\p{Punct}");
+        Matcher matcher = pattern.matcher(var);
+        return matcher.find();
+    }
+
+    public String maj(String var) {
+        String varM = null;
+
+        for (int i = 0; i < var.length(); i++) {
+            if (i == 0) { varM = Character.toString(var.toUpperCase().charAt(0)); } else {
+                varM += var.toLowerCase().charAt(i);
+            }
+        }
+        return varM;
+    }
+
+    public boolean verifMaj(String var) {
+        boolean maj = false;
+        for(int a = 0; a < var.length(); a++) {
+            char pa = var.charAt(a);
+            if(Character.isUpperCase(pa)) {
+                maj = true;
+            }
+        }
+        if(maj) { return true; } else { return false; }
+    }
+
+    public boolean verifSize(String var, Integer minSize) {
+        if(var.length() >= minSize) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean verifFormatTel(String tel) {
+        if(tel.chars().allMatch(Character::isDigit)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}

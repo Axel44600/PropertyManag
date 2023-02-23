228c8ee24186ae8d49bf88121b32aace92e32b2a
= <=>
2023-02-23 15:05:43 +0100

    Added AdminServiceImplTest

---
+++ src/test/java/org/application/propertymanag/service/impl/AdminServiceImplTest.java
@@ -0,0 +1,23 @@
+package org.application.propertymanag.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class AdminServiceImplTest {
+
+    @Test
+    public void getRandomStrTest() {
+        int n = 20;
+        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder s = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            int index = (int)(str.length() * Math.random());
+            s.append(str.charAt(index));
+        }
+
+        assertEquals(s.toString().length(), n);
+    }
+}

92fbb1b2a0496f4901d3543d07768fe01a977acc
= <=>
2023-02-23 14:41:37 +0100

    Added PathConfig

---
+++ src/main/java/org/application/propertymanag/configuration/PathConfig.java
@@ -0,0 +1,10 @@
+package org.application.propertymanag.configuration;
+
+public interface PathConfig {
+
+    String APP_NAME = "PropertyManag";
+    String PATH = "http://localhost";
+    String APP_PATH = "/app";
+    String ADMIN_PATH = "app/admin";
+
+}

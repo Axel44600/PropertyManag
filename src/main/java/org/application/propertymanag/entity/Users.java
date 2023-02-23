cd43b6c93df1d69c5b1ab89f70f91cdd586e9db5
= <=>
2023-02-23 14:20:52 +0100

    Added Users Entity

---
+++ src/main/java/org/application/propertymanag/entity/Users.java
@@ -0,0 +1,81 @@
+package org.application.propertymanag.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users implements UserDetails {
+
+    @Id
+    @SequenceGenerator(name="user_seq", sequenceName="user_seq", allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="user_seq")
+    private Integer id;
+    private String pseudo;
+    private String password;
+    private String nom;
+    private String prenom;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private String registerKey;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return pseudo;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+}

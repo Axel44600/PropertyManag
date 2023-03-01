9f228e5c7ff849c692531cf32b052567d623c3c5
= <=>
2023-03-01 15:44:25 +0100

    Update UserRepository

--- src/main/java/org/application/propertymanag/auth/repository/UserRepository.java
+++ src/main/java/org/application/propertymanag/auth/repository/UserRepository.java
@@ -1,13 +1,20 @@
 package org.application.propertymanag.auth.repository;
 
 import org.application.propertymanag.entity.Users;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
+
+    @Query(value = "SELECT u FROM Users u ORDER BY u.id")
+    @NotNull
+    List<Users> findAll();
 
     Optional<Users> findByPseudo(String pseudo);
 
@@ -15,5 +22,5 @@ public interface UserRepository extends JpaRepository<Users, Integer> {
 
     Optional<Users> findByRegisterKey(String registerKey);
 
-    void delete(Users u);
+    void delete(@NotNull Users u);
 }

3cf2b726b6a0e2f1e8978c52f785426b8abbba3c
= <=>
2023-03-16 09:26:57 +0100

    Update ApplicationConfiguration

--- src/main/java/org/application/propertymanag/configuration/ApplicationConfiguration.java
+++ src/main/java/org/application/propertymanag/configuration/ApplicationConfiguration.java
@@ -11,39 +11,17 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableSwagger2
 public class ApplicationConfiguration {
 
     private final UserRepository repository;
 
     @Bean
-    public Docket docAPI() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder().title("PropertyManag - Outil de gestion de location immobilière")
-                        .description("Documentation de l'api")
-                        .license("fr.propertyManag")
-                        .version("1.0")
-                        .build()
-                ).groupName("PropertyManag V1.0")
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("org.application.propertymanag"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    @Bean
     public UserDetailsService userDetailsService() {
         return username -> repository.findByPseudo(username)
-                .orElseThrow(() -> new UsernameNotFoundException("UserEntity not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("UserEntity introuvable"));
     }
 
     @Bean
